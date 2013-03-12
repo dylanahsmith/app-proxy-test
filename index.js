d061415802c7fcfbd2c41847210d113285174a81
Dylan Smith <Dylan.Smith@shopify.com>
2013-03-12 15:28:40 -0400

    Add the ability to install from the home page.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var http = require('http');
+var https = require('https');
+var querystring = require('querystring')
 var connect = require('connect');
 
 var port = process.env.PORT || 5000;
@@ -49,9 +51,68 @@ function delay(request, response, next) {
   }
 }
 
+function htmlLink(text, url) {
+  if (url) {
+    return '<a href="' + url + '">' + text + '</a>';
+  } else {
+    return text;
+  }
+}
+
 function homepage(request, response, next) {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<html><body>App Proxy Test</body></html>');
+  var authorizeUrl = null;
+
+  if (process.env.SHOPIFY_API_KEY) {
+    var params  = {
+      client_id: process.env.SHOPIFY_API_KEY,
+      scope: 'read_content',
+      redirect_uri: 'http://app-proxy-test.herokuapp.com/install'
+    };
+    authorizeUrl = "https://www.shopify.com/admin/oauth/authorize?" + querystring.stringify(params);
+  }
+  response.end("<html><body><h1>App Proxy Test</h1><p>" + htmlLink("Install", authorizeUrl) + " in your shopify store.</p></body></html>");
+}
+
+function install(request, response, next) {
+  if (!request.query.code || !request.query.shop) {
+    response.writeHead(422);
+    response.end();
+    return;
+  }
+  var params = {
+    client_id: process.env.SHOPIFY_API_KEY,
+    client_secret: process.env.SHOPIFY_API_SECRET,
+    code: request.query.code
+  };
+  var postBody = JSON.stringify(params);
+  var shopifyReq = https.request({
+    host: request.query.shop,
+    method: "POST",
+    path: "/admin/oauth/access_token",
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': postBody.length
+    }
+  }, function(shopifyRes) {
+    if (shopifyRes.statusCode >= 200 && shopifyRes.statusCode < 300) {
+      var buf = '';
+      shopifyRes.setEncoding('utf8');
+      shopifyRes.on('data', function(chunk){ buf += chunk });
+      shopifyRes.on('end', function(){
+        var data = JSON.parse(buf);
+        console.log(request.query.shop + ' access token: ' + data.access_token);
+        response.writeHead(301, {'Location': 'https://www.shopify.com/admin/apps'});
+        response.end();
+      });
+    } else {
+      response.writeHead(shopifyRes.statusCode, {'Content-Type': shopifyRes.headers['content-type']});
+      shopifyRes.pipe(response);
+    }
+    shopifyRes.on('error', next);
+  });
+  shopifyReq.on('error', next);
+  shopifyReq.end(postBody);
 }
 
 function server(port) {
@@ -69,7 +130,8 @@ function server(port) {
   proxy.use('/hangup', hangup);
   proxy.use(plain);
   app.use("/proxy", proxy);
-  app.use(homepage);
+  app.use("/install", install);
+  app.use("/", homepage);
 
   var server = http.createServer(app);
   server.listen(port, function() {
